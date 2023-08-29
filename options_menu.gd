f649b7d3d58a10c5101bb3cc8f836835cee5d12d
sophiestucki <sophie.stucki@epfl.ch>
2023-08-29 21:06:11 +0200

    creation: options + main menu

---
+++ options_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+#TODO
+func _on_volume_pressed():
+	pass # Replace with function body.
+
+#TODO
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
